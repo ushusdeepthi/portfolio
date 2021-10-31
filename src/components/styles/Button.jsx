561dcbb812a041365611ed7d450a09281a46bdea
Deepthi Ushus <ushusdeepthi@gmail.com>
2021-10-31 14:30:53 +0100

    :lipstick: style the button in haeder

---
+++ src/components/styles/Button.jsx
@@ -0,0 +1,13 @@
+import styled, { keyframes } from 'styled-components';
+
+const ButtonStyled = styled.button`
+    color:#402039;
+    background-color:#C8BFC7;
+    text-transform:uppercase;
+    font-size:2rem;
+    padding:1rem 3rem;
+    border-radius:10rem;
+    letter-spacing:0.2rem;
+`
+
+export default ButtonStyled;
