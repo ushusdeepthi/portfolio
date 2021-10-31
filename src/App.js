58fab8b453f7fb55dd81c204f20e5cd250e0b4fb
Deepthi Ushus <ushusdeepthi@gmail.com>
2021-10-31 14:23:58 +0100

    :sparkles: Intoduce new routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+import AboutPage from './pages/AboutPage'
+import ContactPage from './pages/ContactPage'
+import HomePage from './pages/HomePage'
+import PortfolioPage from './pages/PortfolioPage'
+
 
 export default function App() {
   return (
-    <div>
-      Deepthi's portfolio
-    </div>
-  )
+      <Switch>
+        <Route path="/about">
+          <AboutPage />
+        </Route>
+        <Route path="/contact">
+          <ContactPage />
+        </Route>
+        <Route path="/projects">
+          <PortfolioPage />
+        </Route>
+        <Route path="/">
+          <HomePage />
+        </Route>     
+      </Switch>
+
+    )
 }
 
