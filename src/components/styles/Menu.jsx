0626a0ae84de5f61b9f5efbdc2453a417924fc0b
Deepthi Ushus <ushusdeepthi@gmail.com>
2021-11-05 14:22:56 +0100

    :lipstick: Add styling for menu

---
+++ src/components/styles/Menu.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const MenuStyled = styled.div`
+    text-align:center;
+    text-transform:uppercase;
+    background:White;
+    height:100vh;
+    .close-menu{
+        position:absolute;
+        top:0.5rem;
+        right:10rem
+    }
+`
+export default MenuStyled
