0310660fcfed5379bd6959bb9c43010556016f4e
Deepthi Ushus <ushusdeepthi@gmail.com>
2021-11-10 19:40:40 +0100

    :construction: Fix portfolio page structure and styling

---
+++ src/components/styles/Card.jsx
@@ -0,0 +1,54 @@
+import styled, { keyframes } from 'styled-components';
+
+const Card = styled.div `
+    margin:2rem;
+    flex :0 1 20vw;
+    perspective:150rem;
+    -moz-perspective: 150rem;
+    position:relative;    
+    height:50vh;
+    .card__side{
+        border-radius: 3px;
+        width:100%;
+        height:50rem;
+        position:absolute;
+        top:0;
+        left:0;
+        backface-visibility: hidden;
+        
+        box-shadow: 0 1rem 2rem rgba(0,0,0,0.5);
+        transition:all 1s ease;
+
+        &--front{
+            background-color: #fff;        
+        }
+        &--back{
+            background-image: linear-gradient(to right bottom, rgb(48,115,81), rgb(64,32,57));
+            transform:rotateY(180deg);
+            color:#C8BFC7;
+            padding:2rem;
+        }
+    }
+    &:hover .card__side--front{
+        transform:rotateY(180deg);
+    }
+    &:hover .card__side--back {
+        transform: rotateY(0);
+    }
+    .card__img{
+        max-width:100%;
+        /* height:100%; */
+        border-radius:5px;
+    }
+    .card__heading{
+        text-align:center;
+    }
+    .card__action{
+        color:#C8BFC7;
+        text-decoration: none;
+        text-align:center;
+    }
+`
+
+
+export default Card;
--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -1,9 +1,95 @@
 import React from 'react'
+import NavBar from '../components/NavBar'
+import {Container,Wrapper} from '../components/styles/Container'
+import Card from '../components/styles/Card'
+import HeadingSecondary from '../components/styles/HeadingSecondary'
+import quiz from '../assets/images/quiz.jpg'
+import starWars from '../assets/images/star-wars.jpg'
+import yatzy from '../assets/images/yatzy.jpg'
+import crm from '../assets/images/crm.jpg'
+import acme from '../assets/images/acme.jpg'
+import webshop from '../assets/images/webshop.jpg'
 
 export default function PortfolioPage() {
+    const handleClick =()=>window.open('https://ushusdeepthi.github.io/Project/', '_blank');
     return (
-        <div>
-            
-        </div>
+        <Wrapper>
+                <NavBar />
+                <div className="utility">
+                <HeadingSecondary>Portfolio</HeadingSecondary>
+                </div>
+                <Container wrap>
+                    <Card onClick={handleClick}>
+                        
+                        <div className="card__side card__side--front">
+                           <img src={quiz} alt="quiz_project" className="card__img" />
+                        </div>
+                        <div className="card__side card__side--back">
+                            
+                            <h1 className="card__heading">
+                                Quiz App
+                            </h1>
+                            <p className="card__text">
+                                The project includes the developement of a quiz app using object oriented javascript.The questions are fetched from an external api and rendered one by one.The score of the game is provided at the end.
+                            </p>
+                            <ul className="card__list">
+                                <li>Javascript</li>
+                                <li>HTML</li>
+                                <li>CSS</li>
+                            </ul>
+ 
+                            <a  href="https://github.com/ushusdeepthi/Project" className="card__action card__action--code" target="_blank">
+                                source code
+                            </a>
+                            <a href="https://ushusdeepthi.github.io/Project/"className="card__action card__action-app">
+                                app
+                            </a>
+
+                        </div>
+                        
+                    </Card>
+                    <Card>
+                        <div className="card__side card__side--front">
+                            <img src={starWars} alt="star-wars" className="card__img" />
+                        </div>
+                        <div className="card__side card__side--back">
+                            BACK
+                        </div>
+                    </Card>
+                    <Card>
+                        <div className="card__side card__side--front">
+                            <img src={yatzy} alt="Yatzy game" className="card__img" />
+                        </div>
+                        <div className="card__side card__side--back">
+                            BACK
+                        </div>
+                    </Card>
+                    <Card>
+                        <div className="card__side card__side--front">
+                            <img src={crm} alt="React-CRM" className="card__img" />
+                        </div>
+                        <div className="card__side card__side--back">
+                            BACK
+                        </div>
+                    </Card>
+                    <Card>
+                        <div className="card__side card__side--front">
+                            <img src={acme} alt="ACME Events" className="card__img" />
+                        </div>
+                        <div className="card__side card__side--back">
+                            BACK
+                        </div>
+                    </Card>
+                    <Card>
+                        <div className="card__side card__side--front">
+                            <img src={webshop} alt="Webshop" className="card__img" />
+                        </div>
+                        <div className="card__side card__side--back">
+                            BACK
+                        </div>
+                    </Card>
+                </Container>             
+            </Wrapper>        
     )
 }
+
