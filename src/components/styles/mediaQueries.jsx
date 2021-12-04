0c5956f8372ac88c36c095f7ad70d1b6751a6758
Deepthi Ushus <ushusdeepthi@gmail.com>
2021-12-04 17:48:58 +0100

    :iphone: edit media queries

--- src/components/styles/mediaQueries.jsx
+++ src/components/styles/mediaQueries.jsx
@@ -1,8 +1,14 @@
 export const breakpoints={
-    xs:37.5,
-    sm:56.25,
-    md:75,
-    xl:112.5
+    // xxs:25,
+    // xs:37.5, //<600
+    // sm:56.25, //<900
+    // md:75, //900 to 1200px
+    // xl:112.5 //>1800px
+    xxs:20,//320px,
+    xs:30,//480px, 
+    sm:48,//768px
+    md:64, //1024px
+    lg:75 //1200px
 }
 export const mediaQueries = (key)=>{
     return (style) =>`@media (max-width: ${breakpoints[key]}em) {${style}}`;
